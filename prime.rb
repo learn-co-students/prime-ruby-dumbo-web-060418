8bf796d20d3c1ac0df3ccc268b962ac129d48472
Katrina Garloff <katrinagarloff@gmail.com>
2018-05-17 19:27:20 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
 # Add  code here!
+def prime? (is_prime) 
+  true_false = true
+  ar = []
+  
+  for i in 2..is_prime-1
+  ar.push(i)
+end
+
+if is_prime > 1 
+ar.each do |i|
+  if is_prime % i == 0 
+    true_false = false
+end
+end
+else 
+  true_false = false
+end
+return true_false
+end
+
